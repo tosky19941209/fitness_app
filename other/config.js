3d0f70873246388d1a84435731a2a7b2e39e5119
felixeliass1994 <felixeliass1994@gmail.com>
2024-06-25 15:22:39 +0900

    mongo

--- other/config.js
+++ other/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: 8080,
     secret_key: "tlkwjdqhd",
-    mongodb:'mongodb://127.0.0.1:27017/fitness-database',
+    // mongodb:'mongodb://127.0.0.1:27017/fitness-database',
     // mongodb:'mongodb://146.59.157.53:27017/fitness-database',
-    // mongodb:'mongodb+srv://jinxiao19941209:rNfoWdcmIlnfvddR@cluster0.j8ekrte.mongodb.net/',
+    mongodb:'mongodb+srv://jinxiao19941209:rNfoWdcmIlnfvddR@cluster0.j8ekrte.mongodb.net/',
 }
